7b1912fb4c2d7e1fc0352eda02345694c38ef66a
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-09-29 11:11:17 +0530

    [Shubham]Added: Firstname validation

--- src/com/capgi/UserRegistration.java
+++ src/com/capgi/UserRegistration.java
@@ -1,8 +1,32 @@
 package com.capgi;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
-	public static void main(String[] args) {
-		System.out.println("Welcome to the user Registration");
+	public static boolean ValidateFirstName(String fName) {
+		Pattern pattern = Pattern.compile("([A-Z]{1})([a-zA-Z]{2,})");
+		Matcher matcher = pattern.matcher(fName);
+		boolean isValid = matcher.find();
+		if (isValid) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the first name");
+		String fName = sc.nextLine();
+		if (ValidateFirstName(fName)) {
+			System.out.println("Name is valid");
+
+		} else {
+			System.out.println("Name is invalid");
+		}
+
+	}
 }
