91021d48907edc6d85d00cfd0ae6ca0e15f202f4
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-09-29 13:06:23 +0530

    [Shubham]Refactored:Added all past methods in UC4

--- src/com/capgi/UserRegistration.java
+++ src/com/capgi/UserRegistration.java
@@ -6,8 +6,49 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-	public void ValidatePhoneNo(String phoneNo) {
+	public void ValidateFirstName(String fName) {
 
+		Pattern pattern = Pattern.compile("([A-Z]{1})([a-zA-Z]{2,})");
+		Matcher matcher = pattern.matcher(fName);
+		boolean isValid = matcher.find();
+		if (isValid) {
+			System.out.println("Name is valid");
+			;
+		} else {
+			System.out.println("Name is invalid");
+
+		}
+	}
+
+	public void ValidateLastName(String lName) {
+		Pattern pattern = Pattern.compile("([A-Z]{1})([a-zA-Z]{2,})");
+		Matcher matcher = pattern.matcher(lName);
+		boolean isValid = matcher.find();
+		if (isValid) {
+			System.out.println("Name is valid");
+			;
+		} else {
+			System.out.println("Name is invalid");
+
+		}
+	}
+
+	public void ValidateEmail(String email) {
+		Pattern pattern = Pattern
+				.compile("(^([a-z]+)([+._-]{0,1})([0-9a-z]*)([@]{1})([a-z0-9]+)(.([a-z]{2,})){1}(.[a-z][a-z])?$)");
+		Matcher matcher = pattern.matcher(email);
+		boolean isValid = matcher.find();
+		if (isValid) {
+			System.out.println("email is valid");
+			;
+		} else {
+			System.out.println("email is invalid");
+
+		}
+
+	}
+
+	public void ValidatePhoneNo(String phoneNo) {
 		Pattern pattern = Pattern.compile("(^([0-9]+)( )([1-9]{1})([0-9]{9})$)");
 		Matcher matcher = pattern.matcher(phoneNo);
 		boolean isValid = matcher.find();
